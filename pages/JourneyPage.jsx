d08e44183066d4d299cdce17edb0d74804267282
briancosentino <cosentinobrian@gmail.com>
2025-04-28 11:57:44 +0200

    Add SearchBar component to JourneyPage for participant filtering functionality

--- pages/JourneyPage.jsx
+++ pages/JourneyPage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import journeys from '../src/data/journeys';
 import { useState } from 'react';
+import SearchBar from '../src/components/SearchBar';
 
 const JourneyPage = () => {
     const { id } = useParams();
     const thisJourney = journeys.find(journey => journey.id == id)
     console.log(thisJourney);
-    const partecipants = thisJourney.people
+    const [partecipants, setPartecipants] = useState(thisJourney.people)
+    const [searchedPartecipants, setSearchedPartecipants] = useState(partecipants)
     console.log(partecipants);
     const [dropdownOpen, setDropdownOpen] = useState(true)
     const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
@@ -16,10 +18,14 @@ const JourneyPage = () => {
         <>
             <div>
                 <h1 className="display-3 text-center">{thisJourney.destination}</h1>
-                <h2 className='mb-5'>Partecipanti</h2>
+                <div className='d-flex justify-content-between align-items-center'>
+
+                    <h2 className='mb-5'>Partecipanti</h2>
+                    <SearchBar setSearchedPartecipants={setSearchedPartecipants} partecipants={partecipants} />
+                </div>
                 <div className="row flex-wrap g-3  ">
 
-                    {partecipants.map(partecipant => (
+                    {searchedPartecipants.map(partecipant => (
                         <div key={partecipant.codiceFiscale} className="accordion " id={`accordion-${partecipant.codiceFiscale}`}>
                             <div className="accordion-item">
                                 <h2 className="accordion-header">
@@ -32,6 +38,7 @@ const JourneyPage = () => {
                                         <ul className="list-">
                                             <li className='list-group-item' ><strong>Numero di telefono: </strong>{partecipant.phoneNumber}</li>
                                             <li className='list-group-item' ><strong>Email: </strong>{partecipant.email}</li>
+                                            <li className='list-group-item'><strong>Codice fiscale: </strong>{partecipant.codiceFiscale}</li>
                                             <li className='list-group-item' ><strong>Contatto di emergenza: </strong>{partecipant.emergencyContactPhone}</li>
 
 
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react"
+import journeys from "../data/journeys"
+
+
+const SearchBar = ({ partecipants, setSearchedPartecipants }) => {
+    const [isOpen, setIsOpen] = useState(false)
+    const [searched, setSearched] = useState(' ')
+
+
+    const handleChanges = useEffect(() => {
+        if (searched !== ' ') {
+
+            setSearchedPartecipants(partecipants.filter(partecipant => partecipant.name.toLowerCase().includes(searched.toLowerCase())))
+        } else {
+            console.log('la ricerca è nulla ');
+
+
+            setSearchedPartecipants(partecipants)
+        }
+
+
+    }, [searched])
+
+    const handleChange = (e) => {
+        setSearched(e.target.value)
+
+
+    }
+    console.log(searched.length);
+    console.log(partecipants);
+
+
+
+
+    return (
+        <>
+            <button className={`btn ${isOpen ? 'd-none' : ''}`} onClick={() => setIsOpen(true)}><i className="fa-solid fa-magnifying-glass"></i></button>
+            {isOpen && (
+                <>
+                    <input onChange={handleChange} className="p-2 rounded-4 border border-secondary ps-3 pe-5" type="search" name="filter" id="filter" placeholder="Cerca partecipanti..." />
+                </>
+            )}
+        </>
+
+    )
+}
+
+export default SearchBar
