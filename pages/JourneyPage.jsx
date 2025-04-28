fed931c1f6af49f67c48995f438a5d65d138cab0
briancosentino <cosentinobrian@gmail.com>
2025-04-28 10:38:35 +0200

    Refactor JourneyPage to include accordion for participant details and add Bootstrap JS to index.html

--- pages/JourneyPage.jsx
+++ pages/JourneyPage.jsx
@@ -13,28 +13,37 @@ const JourneyPage = () => {
     const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
 
     return (
-        <div>
-            <h1 className="display-3 text-center">{thisJourney.destination}</h1>
-            <h2 className='mb-5'>Partecipanti</h2>
-            <div className="row flex-wrap g-3  ">
+        <>
+            <div>
+                <h1 className="display-3 text-center">{thisJourney.destination}</h1>
+                <h2 className='mb-5'>Partecipanti</h2>
+                <div className="row flex-wrap g-3  ">
 
-                {partecipants.map(partecipant => (
-                    <div key={partecipant.codiceFiscale} className="col-3">
-
-                        <div className=" border border-primary rounded-3 p-3 ">
-                            <div className="">
-
-                                <div className="card-title position-relative text-center h5 fw-semibold">{partecipant.name} {partecipant.surname} <span className='position-absolute end-0 top-50 h6 text-primary' style={{ transform: 'translate(-50%, -50%)' }}><i className='fa-solid fa-chevron-down'></i></span></div>
-
+                    {partecipants.map(partecipant => (
+                        <div key={`accordion-${partecipant.name}-${partecipant.codiceFiscale}`} className="accordion" id={`accordion-${partecipant.name}-${partecipant.id}`}>
+                            <div className="accordion-item">
+                                <h2 className="accordion-header">
+                                    <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={`collapse-${partecipant.name}-${partecipant.id}`} aria-expanded="true" aria-controls="collapseOne">
+                                        {partecipant.name} {partecipant.surname}
+                                    </button>
+                                </h2>
+                                <div id={`collapse-${partecipant.name}-${partecipant.id}`} className="accordion-collapse collapse show" data-bs-parent={`accordion-${partecipant.name}-${partecipant.id}`}>
+                                    <div className="accordion-body">
+                                        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
 
-                ))}
+                        </div>
+
+                    ))}
+
+                </div>
+
+
             </div>
 
-
-        </div>
+        </>
     )
 }
 
